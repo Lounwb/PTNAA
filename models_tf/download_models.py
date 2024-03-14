a8133e5e29aa0c64f3ef918ef0f6766178978ceb
Lounwb <1904396849@qq.com>
2024-03-14 13:42:23 +0800

    Update download_models.py

--- models_tf/download_models.py
+++ models_tf/download_models.py
@@ -1,7 +1,6 @@
 import urllib.request
 import time
 
-# 下载地址列表
 download_list = [
     "http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz",
     "http://download.tensorflow.org/models/inception_v4_2016_09_09.tar.gz",
@@ -19,12 +18,9 @@ download_list = [
     "http://download.tensorflow.org/models/ens_adv_inception_resnet_v2_2017_08_18.tar.gz"
 ]
 
-# 遍历下载地址列表
 for url in download_list:
-    # 提取文件名
     filename = url.split("/")[-1]
 
-    # 下载文件
     print(f"[{time.asctime(time.localtime())}] Downloading", filename)
     try:
         urllib.request.urlretrieve(url, filename)
